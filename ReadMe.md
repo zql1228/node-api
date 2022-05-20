692a6c6eea8324fd7f7bb39ca0ebab3bae42f69f
zql1228 <2053221503@qq.com>
2022-05-20 15:32:04 +0800

    项目基础搭建

--- ReadMe.md
+++ ReadMe.md
@@ -4,3 +4,9 @@ npm init -y
 2 git 初始化
 git init
 生成.git 隐藏文件夹
+3 创建 ReadMe 文件
+二、搭建项目
+1 按装 Koa 框架
+npm i koa 2.编写最基本的 app
+创建 src/main.js
+代码粘贴
---
+++ src/main.js
@@ -0,0 +1,8 @@
+const Koa = require('koa')
+const app = new Koa()
+app.use((ctx, next) => {
+  ctx.body = 'hello world'
+})
+app.listen(3000, () => {
+  console.log('server is running on http://localhost:3000')
+})
